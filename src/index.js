a758f1de908e8829f9409e03e2259546f05f3d54
Maksim Palagin <m.palagin91@gmail.com>
2023-01-26 11:16:30 +0700

    feat: implement function for checking brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  const configObj = Object.fromEntries(bracketsConfig);
+
+  const temp = [];
+
+  for (let bracket of str) {
+    console.log(temp);
+  
+    if (Object.values(configObj).indexOf(bracket) > -1) {
+      if (bracket !== configObj[temp[temp.length - 1]]) {
+        if (configObj[bracket] === bracket) {
+          temp.push(bracket);
+        }
+        else {
+          console.log(false, 'condition');
+          return false;
+        }
+      }
+      else {
+        temp.pop();
+       } 
+    }
+    else {
+      temp.push(bracket);
+    }
+  }
+
+  return temp.length === 0;
+};
